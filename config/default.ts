b50af224fc16bc6ce39a0db1671238f0d919a7eb
Shivam Upadhyay <shivam.upadhyay@codinova.com>
2019-03-28 10:52:36 +0530

    b name modification

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default {
     HOST: 'http//localhost',
     PORT: 8080,
-    MONGO_URI: 'mongodb://localhost:27017/codinova',
+    MONGO_URI: 'mongodb://localhost:27017/todo',
     JWT: 'myS3cR8TKey',
 }
